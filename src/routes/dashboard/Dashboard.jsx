385c4d4288acfb1f5a88c92a29f0c790cbd90412
Yusupov Elbek <elbeky829@gmail.com>
2023-10-17 03:30:05 +0500

    fixing bug3

--- src/routes/dashboard/Dashboard.jsx
+++ src/routes/dashboard/Dashboard.jsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
 import "./Dashboard.scss";
 import { dashLogo } from "../../assets/img";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import {
   MdOutlineSpaceDashboard,
   MdLogout,
@@ -9,32 +11,46 @@ import {
 } from "react-icons/md";
 
 const Dashboard = () => {
-  function LogOut(){
-    localStorage.clear()
-    window.location.reload()
+  function LogOut() {
+    localStorage.clear();
+    toast.info("Please refresh site to logout", {
+      theme: "colored",
+    });
   }
   return (
     <nav>
+      <ToastContainer />
       <div className="container">
         <div className="nav__wrapper">
           <div className="dash__logo">
             <img src={dashLogo} alt="" />
           </div>
           <div className="dash__links">
-            <NavLink to='/' className={({isActive})=>(isActive?"active":"incative")}>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "incative")}
+            >
               <MdOutlineSpaceDashboard />
               Dashboard
             </NavLink>
-            <NavLink to='/add-student' className={({isActive})=>(isActive?"active":"incative")}>
+            <NavLink
+              to="/add-student"
+              className={({ isActive }) => (isActive ? "active" : "incative")}
+            >
               <MdOutlinePersonAddAlt1 />
               Add Student
             </NavLink>
-            <NavLink to='/profile' className={({isActive})=>(isActive?"active":"incative")}>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) => (isActive ? "active" : "incative")}
+            >
               <MdOutlineContactEmergency />
               Profile
             </NavLink>
           </div>
-          <button onClick={()=>LogOut()}><MdLogout/> Logout</button>
+          <button onClick={() => LogOut()}>
+            <MdLogout /> Logout
+          </button>
         </div>
       </div>
     </nav>
